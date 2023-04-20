9ca372c867ef2d76a63fbbb64edeac8f028e298c
DESKTOP-VCBUV18\이정민 <jungmin5789@gmail.com>
2023-04-20 22:36:07 +0900

    타이틀 화면 돌아가는 안내문구 변경

--- tyrano/lang.js
+++ tyrano/lang.js
@@ -1,1 +1,29 @@
-window.tyrano_lang={"word":{"go_title":"Back to Title Screen?","exit_game":"게임을 종료하시겠습니까?","not_saved":"빈 슬롯입니다.","tag":"Tag name","not_exists":"could not be found","error":"An error occurred. check the scenario script.","label":"The label","label_double":"has more than one occurrence in the scene file","error_occurred":"An error occurred. check the scenario script.","save_file_violation_1":"Save data detected. Only load data from a trusted source!","save_file_violation_2":"Load the save data and start the game?","save_file_violation_3":"Start-up aborted. Please delete the save data and restart.","reload":"reload"},"novel":{"file_menu_button_save":"menu_button_save.gif","file_menu_button_load":"menu_button_load.gif","file_menu_button_message_close":"menu_message_close.gif","file_menu_button_skip":"menu_button_skip.gif","file_menu_button_title":"menu_button_title.gif","file_menu_button_close":"menu_button_close.png","file_menu_bg":"menu_bg.jpg","file_save_bg":"menu_save_bg.jpg","file_load_bg":"menu_load_bg.jpg","file_button_menu":"nextpage.gif"}}
+window.tyrano_lang={
+	"word":{
+		"go_title":"타이틀 화면으로 돌아가시겠습니까?",
+		"exit_game":"게임을 종료하시겠습니까?",
+		"not_saved":"빈 슬롯입니다.",
+		"tag":"Tag name",
+		"not_exists":"없다 닝겐. 찾을 수 없다 닝겐.",
+		"error":"An error occurred. check the scenario script.",
+		"label":"The label",
+		"label_double":"has more than one occurrence in the scene file",
+		"error_occurred":"An error occurred. check the scenario script.",
+		"save_file_violation_1":"Save data detected. Only load data from a trusted source!",
+		"save_file_violation_2":"Load the save data and start the game?",
+		"save_file_violation_3":"Start-up aborted. Please delete the save data and restart.",
+		"reload":"reload"
+	},
+	"novel":{
+		"file_menu_button_save":"menu_button_save.gif",
+		"file_menu_button_load":"menu_button_load.gif",
+		"file_menu_button_message_close":"menu_message_close.gif",
+		"file_menu_button_skip":"menu_button_skip.gif",
+		"file_menu_button_title":"menu_button_title.gif",
+		"file_menu_button_close":"menu_button_close.png",
+		"file_menu_bg":"menu_bg.jpg",
+		"file_save_bg":"menu_save_bg.jpg",
+		"file_load_bg":"menu_load_bg.jpg",
+		"file_button_menu":"Text_Icon_01.gif"
+	}
+}
