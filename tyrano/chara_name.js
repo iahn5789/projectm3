58d602c84793c49e7193be609c19ae3262a44949
DESKTOP-VCBUV18\이정민 <jungmin5789@gmail.com>
2023-04-23 21:05:13 +0900

    24 : 블러기능 주석 처리

--- tyrano/chara_name.js
+++ tyrano/chara_name.js
@@ -146,107 +146,107 @@ document.addEventListener('click', function(event) {
 
 
 
-      // Get the Save button element by its name attribute
-  const menuButtons = document.getElementsByClassName("menu_button");
-  const roleButtons = document.getElementsByClassName("rol_button");
-  const layerMenu = document.getElementsByClassName("layer_menu")[0];
-  const hideButton = document.getElementsByClassName("hide_btn")[0];
-  const gameLayer = document.getElementsByClassName("root_layer_game")[0];
-  const message_inner = document.getElementsByClassName("message_inner")[0];
-  const message_outer = document.getElementsByClassName("message_outer")[0];
-  const chara_name_area = document.getElementsByClassName("chara_name_area")[0];
-  // Add a click event listener to the Save button
+//       // Get the Save button element by its name attribute
+//   const menuButtons = document.getElementsByClassName("menu_button");
+//   const roleButtons = document.getElementsByClassName("rol_button");
+//   const layerMenu = document.getElementsByClassName("layer_menu")[0];
+//   const hideButton = document.getElementsByClassName("hide_btn")[0];
+//   const gameLayer = document.getElementsByClassName("root_layer_game")[0];
+//   const message_inner = document.getElementsByClassName("message_inner")[0];
+//   const message_outer = document.getElementsByClassName("message_outer")[0];
+//   const chara_name_area = document.getElementsByClassName("chara_name_area")[0];
+//   // Add a click event listener to the Save button
 
-  for(let i = 0 ; i< menuButtons.length; i++){
+//   for(let i = 0 ; i< menuButtons.length; i++){
     
-if(i !== 5){
-    menuButtons[i].addEventListener("click", function() {
-      // Get the root_layer_game element by its class name
+// if(i !== 5){
+//     menuButtons[i].addEventListener("click", function() {
+//       // Get the root_layer_game element by its class name
     
 
-      // Add the "filter: blur(5px)" style to the game layer
-      gameLayer.style.filter = "blur(5px)";
-      message_inner.style.filter = "blur(5px)";
-      message_outer.style.filter = "blur(5px)";
-      chara_name_area.style.filter = "blur(5px)";
-      hideButton.style.filter = "blur(5px)";
-      // roleButtons.style.filter = "blur(5px)";
+//       // Add the "filter: blur(5px)" style to the game layer
+//       gameLayer.style.filter = "blur(5px)";
+//       message_inner.style.filter = "blur(5px)";
+//       message_outer.style.filter = "blur(5px)";
+//       chara_name_area.style.filter = "blur(5px)";
+//       hideButton.style.filter = "blur(5px)";
+//       // roleButtons.style.filter = "blur(5px)";
 
 
-      for(let j = 0 ; j < menuButtons.length; j++){
+//       for(let j = 0 ; j < menuButtons.length; j++){
 
-        menuButtons[j].style.filter = "blur(5px)";
+//         menuButtons[j].style.filter = "blur(5px)";
         
-      }
+//       }
 
       
-      for(let k = 0 ; k < roleButtons.length; k++){
+//       for(let k = 0 ; k < roleButtons.length; k++){
 
-        roleButtons[k].style.filter = "blur(5px)";
+//         roleButtons[k].style.filter = "blur(5px)";
         
-      }
+//       }
 
-    });
-  }
-}
+//     });
+//   }
+// }
 
-// const roleButtonsArray = Array.from(document.querySelectorAll(".rol_button"));
+// // const roleButtonsArray = Array.from(document.querySelectorAll(".rol_button"));
 
-// // Apply a click event listener to each button
-// roleButtonsArray.forEach(function (button, index) {
-//   button.addEventListener("click", function () {
-//     console.log("Button " + index + " was clicked");
-//     // Add your code to handle the button click event here
-//   });
-// });
-
-
-layerMenu.addEventListener("click",  function(event) {
-
-    // 클릭 이벤트를 발생시킨 요소가 menu_close 버튼인지 체크합니다.
-    if (event.target.src && event.target.src.includes('UI_Close_Bt_02.png','Load_Bt_02.png')) {
-      gameLayer.style.filter = "";
-      message_inner.style.filter = "";
-      message_outer.style.filter = "";
-      chara_name_area.style.filter = "";
-      hideButton.style.filter = "";
+// // // Apply a click event listener to each button
+// // roleButtonsArray.forEach(function (button, index) {
+// //   button.addEventListener("click", function () {
+// //     console.log("Button " + index + " was clicked");
+// //     // Add your code to handle the button click event here
+// //   });
+// // });
 
 
-      for(let j = 0 ; j < menuButtons.length; j++){
+// layerMenu.addEventListener("click",  function(event) {
 
-        menuButtons[j].style.filter = "";
+//     // 클릭 이벤트를 발생시킨 요소가 menu_close 버튼인지 체크합니다.
+//     if (event.target.src && event.target.src.includes('UI_Close_Bt_02.png','Load_Bt_02.png')) {
+//       gameLayer.style.filter = "";
+//       message_inner.style.filter = "";
+//       message_outer.style.filter = "";
+//       chara_name_area.style.filter = "";
+//       hideButton.style.filter = "";
 
-      }
 
-      for(let k = 0 ; k < roleButtons.length; k++){
+//       for(let j = 0 ; j < menuButtons.length; j++){
 
-        roleButtons[k].style.filter = "";
+//         menuButtons[j].style.filter = "";
+
+//       }
+
+//       for(let k = 0 ; k < roleButtons.length; k++){
+
+//         roleButtons[k].style.filter = "";
         
-      }
+//       }
 
       
 
-    }
-    // else if (event.target.src && event.target.src.includes('UI_Close_Bt_02.png')) { 
-    //   gameLayer.style.filter = "";
-    //   message_inner.style.filter = "";
-    //   message_outer.style.filter = "";
-    //   chara_name_area.style.filter = "";
+//     }
+//     // else if (event.target.src && event.target.src.includes('UI_Close_Bt_02.png')) { 
+//     //   gameLayer.style.filter = "";
+//     //   message_inner.style.filter = "";
+//     //   message_outer.style.filter = "";
+//     //   chara_name_area.style.filter = "";
     
     
-    //   for(let j = 0 ; j < menuButtons.length; j++){
+//     //   for(let j = 0 ; j < menuButtons.length; j++){
     
-    //     menuButtons[j].style.filter = "";
-    
-    
-    //   }
+//     //     menuButtons[j].style.filter = "";
     
     
-    // } 
+//     //   }
+    
+    
+//     // } 
     
 
 
-})
+// })
 
 
 
