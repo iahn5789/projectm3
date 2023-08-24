4e80b9b1a65247027981c62153ecc75d1f6fec3d
이정민 <jungmin5789@gmail.com>
2023-08-25 03:58:32 +0900

    타이틀-게임노트 연결

---
+++ projectm3/projectm3/Assets/Common/Scripts/LoadGamenote.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGamenote : MonoBehaviour
+{
+    public GameObject gamenoteUIPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void GoGameScene()
+    {
+        // 씬 로드
+        SceneManager.LoadScene(1);
+
+        // GamenoteUI 프리팹 인스턴스화 및 활성화
+        if (gamenoteUIPrefab != null)
+        {
+            gamenoteUIPrefab.SetActive(true);
+        }
+    }
+}
+
+
