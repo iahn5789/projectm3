dcccefcec494b84f7725c45d201035db91174ef0
이정민 <126859438+iahn5789@users.noreply.github.com>
2023-08-28 12:12:20 +0900

    .

--- projectm3/projectm3/Assets/Common/Scripts/LoadGamenote.cs
+++ projectm3/projectm3/Assets/Common/Scripts/LoadGamenote.cs
@@ -7,23 +7,27 @@ public class LoadGamenote : MonoBehaviour
 {
     public GameObject gamenoteUIPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public void GoGameScene()
     {
+        // 이전 씬의 게임 오브젝트들을 비활성화
+        DeactivatePreviousScene();
+
         // 씬 로드
         SceneManager.LoadScene(1);
 
         // GamenoteUI 프리팹 인스턴스화 및 활성화
         if (gamenoteUIPrefab != null)
         {
-            gamenoteUIPrefab.SetActive(true);
+            Instantiate(gamenoteUIPrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+
+    private void DeactivatePreviousScene()
+    {
+        Scene previousScene = SceneManager.GetActiveScene();
+        foreach (GameObject obj in previousScene.GetRootGameObjects())
+        {
+            obj.SetActive(false);
         }
     }
 }
-
-
