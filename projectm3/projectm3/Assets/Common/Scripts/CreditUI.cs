c0e5cef78a065fd7c9a9ce08a43d3d3be51d8455
이정민 <jungmin5789@gmail.com>
2023-09-07 23:48:27 +0900

    CreditUI 생성, GamenoteUI<>CreditUI 연결

---
+++ projectm3/projectm3/Assets/Common/Scripts/CreditUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditUI : MonoBehaviour
+{
+    public GameObject We;
+    public GameObject Help;
+    public GameObject Spon;
+    public Button Button_We;
+    public Button Button_Help;
+    public Button Button_Spon;
+
+    private void Start()
+    {
+        // Button_We 클릭 시 We를 활성화하고 다른오브젝트를 비활성화합니다.
+        Button_We.onClick.AddListener(() =>
+        {
+            We.SetActive(true);
+            Help.SetActive(false);
+            Spon.SetActive(false);
+        });
+
+        // Button_Help 클릭 시 Help를 활성화하고 다른오브젝트를 비활성화합니다.
+        Button_Help.onClick.AddListener(() =>
+        {
+            We.SetActive(false);
+            Help.SetActive(true);
+            Spon.SetActive(false);
+        });
+
+        // Button_Spon 클릭 시 Spon를 활성화하고 다른오브젝트를 비활성화합니다.
+        Button_Spon.onClick.AddListener(() =>
+        {
+            We.SetActive(false);
+            Help.SetActive(false);
+            Spon.SetActive(true);
+        });
+    }
+}
