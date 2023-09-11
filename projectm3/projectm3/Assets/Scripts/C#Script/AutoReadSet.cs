51d73ce143f68160dd44e002aa40be20a7f5ae83
KangSeokChan <ksc7299@naver.com>
2023-09-12 00:48:37 +0900

    AutoRead Success
    AutoRead Success 멈추기까지 완료

---
+++ projectm3/projectm3/Assets/Scripts/C#Script/AutoReadSet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Naninovel;
+using Naninovel.Commands;
+
+public class AutoReadSet : MonoBehaviour
+{
+    private IScriptPlayer scriptPlayer;
+
+    public void OnClick()
+    {
+        scriptPlayer = Engine.GetService<IScriptPlayer>();
+        scriptPlayer.SetAutoPlayEnabled(false);
+    }
+}
