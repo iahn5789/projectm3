c4e38fb0ebe70b2a011e8ef32885a24d2bf91324
KangSeokChan <ksc7299@naver.com>
2023-09-13 22:05:18 +0900

    전체화면 기능 완료(alt+enter) 가능
    전체화면 기능 완료(alt+enter) 가능

---
+++ projectm3/projectm3/Assets/Scripts/C#Script/ScreenModeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenModeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void FullScreenBtOnClick(){
+        SetResolution(true,1920,1080);
+    }
+    public void WinScreenBtOnClick(){
+
+        SetResolution(false,1600,900);
+    }
+
+    public void SetResolution(bool fullscreen, int setWidth, int setHeight)
+    {
+        Screen.SetResolution(setWidth, setHeight, fullscreen);
+    }
+}
