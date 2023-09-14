8c11ab3e511f46e04559e6ddb30e861839cbc2bd
KangSeokChan <ksc7299@naver.com>
2023-09-14 22:15:26 +0900

    SettingUI 토글 애니메이션 설정

--- projectm3/projectm3/Assets/Common/Scripts/ToggleAnimation.cs
+++ projectm3/projectm3/Assets/Common/Scripts/ToggleAnimation.cs
@@ -1,20 +1,47 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Naninovel;
+using Naninovel.UI;
 
 public class ToggleAnimation : MonoBehaviour
 {
     public Animator animator;
-
     private Toggle toggle;
+
+    public bool Status;
+    private IScriptPlayer player;
 
     private void Start()
     {
+        Status = false;
         toggle = GetComponent<Toggle>();
         toggle.onValueChanged.AddListener(OnToggleValueChanged);
+        player = Engine.GetService<IScriptPlayer>();
     }
 
-    private void OnToggleValueChanged(bool isOn)
+    public void OnToggleValueChanged(bool isOn)
     {
-        animator.SetBool("IsOn", isOn); // "IsOn" 파라미터 값을 변경하여 애니메이션 트리거
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ToggleBtClick(toggle.isOn);
+    }
+
+    public void ToggleBtClick(bool isOn)
+    {
+        if (Status && !animator.GetCurrentAnimatorStateInfo(0).IsName("Skip_Toggle_On") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+        {
+            Status = false;
+            animator.SetTrigger("isOn");
+            player.SkipMode = (PlayerSkipMode)0;
+        }
+        else if (!Status && !animator.GetCurrentAnimatorStateInfo(0).IsName("Skip_Toggle_Off") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+        {
+            Status = true;
+            animator.SetTrigger("isOff");
+            player.SkipMode = (PlayerSkipMode)1;
+        }
     }
 }
