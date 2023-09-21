6baf4d9ed3c48af9fd35fe98ab0798b7e0e776b9
이정민 <jungmin5789@gmail.com>
2023-09-22 02:08:24 +0900

    GiftshopUI 패널 애니메이션 추가

---
+++ projectm3/projectm3/Assets/Common/Scripts/GiftshopUI.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GiftshopUI : MonoBehaviour
+{
+    public GameObject KangPanel;
+    public GameObject JinPanel;
+    public GameObject SulPanel;
+    public Button Button_Kang;
+    public Button Button_Jin;
+    public Button Button_Sul;
+
+    private void Start()
+    {
+        // Button_Kang 클릭 시 KangPanel를 활성화하고 다른 오브젝트를 비활성화합니다.
+        Button_Kang.onClick.AddListener(() =>
+        {
+            KangPanel.SetActive(true);
+            JinPanel.SetActive(false);
+            SulPanel.SetActive(false);
+            
+            // Button_Kang의 자식 오브젝트 중 'Blur' 오브젝트를 찾아 활성화
+            ToggleBlurOn(Button_Kang, true);
+            ToggleBlurOn(Button_Jin, false);
+            ToggleBlurOn(Button_Sul, false);
+        });
+
+        // Button_Jin 클릭 시 JinPanel를 활성화하고 다른 오브젝트를 비활성화합니다.
+        Button_Jin.onClick.AddListener(() =>
+        {
+            KangPanel.SetActive(false);
+            JinPanel.SetActive(true);
+            SulPanel.SetActive(false);
+            
+            // Button_Jin의 자식 오브젝트 중 'Blur' 오브젝트를 찾아 활성화
+            ToggleBlurOn(Button_Kang, false);
+            ToggleBlurOn(Button_Jin, true);
+            ToggleBlurOn(Button_Sul, false);
+        });
+
+        // Button_Sul 클릭 시 SulPanel를 활성화하고 다른 오브젝트를 비활성화합니다.
+        Button_Sul.onClick.AddListener(() =>
+        {
+            KangPanel.SetActive(false);
+            JinPanel.SetActive(false);
+            SulPanel.SetActive(true);
+            
+            // Button_Sul의 자식 오브젝트 중 'Blur' 오브젝트를 찾아 활성화
+            ToggleBlurOn(Button_Kang, false);
+            ToggleBlurOn(Button_Jin, false);
+            ToggleBlurOn(Button_Sul, true);
+        });
+    }
+
+    // 자식 오브젝트 중 'Blur' 오브젝트를 찾아 활성화/비활성화
+    private void ToggleBlurOn(Button button, bool isActive)
+    {
+        Transform blurTransform = button.transform.Find("Blur");
+        if (blurTransform != null)
+        {
+            blurTransform.gameObject.SetActive(isActive);
+        }
+    }
+}
---
+++ projectm3/projectm3/Assets/Common/Scripts/ShowHideWithAnimaiton.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections; // 이 부분을 추가
+
+
+public class ShowHideWithAnimation : MonoBehaviour
+{
+    public GameObject objectToShowHide; // 애니메이션을 적용할 대상 오브젝트
+    public Animator animator; // 대상 오브젝트의 Animator 컴포넌트
+
+    private void Start()
+    {
+        // 초기에 대상 오브젝트를 비활성화합니다.
+        objectToShowHide.SetActive(false);
+    }
+
+    // 오브젝트를 표시하고 애니메이션을 재생합니다.
+    public void ShowObject()
+    {
+        if (objectToShowHide != null)
+        {
+            objectToShowHide.SetActive(true);
+
+            // 애니메이션 트리거를 활성화하여 "Show" 애니메이션을 재생합니다.
+            if (animator != null)
+            {
+                animator.SetTrigger("Show");
+            }
+        }
+    }
+
+    // 오브젝트를 숨기고 애니메이션을 재생합니다.
+    public void HideObject()
+    {
+        if (objectToShowHide != null)
+        {
+            // 애니메이션 트리거를 활성화하여 "Hide" 애니메이션을 재생합니다.
+            if (animator != null)
+            {
+                animator.SetTrigger("Hide");
+            }
+
+            // 애니메이션이 끝나면 오브젝트를 비활성화합니다.
+            StartCoroutine(DisableObjectAfterAnimation());
+        }
+    }
+
+    // 애니메이션 재생 후 오브젝트를 비활성화하는 함수
+    private IEnumerator DisableObjectAfterAnimation()
+    {
+        // "Hide" 애니메이션이 종료될 때까지 대기
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // 오브젝트를 비활성화
+        objectToShowHide.SetActive(false);
+    }
+}
