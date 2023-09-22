3fba67a83f4567fe3255edff5efbf216df290172
KangSeokChan <ksc7299@naver.com>
2023-09-23 00:49:11 +0900

    선물상점 UI 설정 Manager

---
+++ projectm3/projectm3/Assets/Scripts/C#Script/BuyGiftUIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyGiftUIManager : MonoBehaviour
+{
+    public Image giftImage;
+    public Text priceText;
+    public Text likeAbilityText;
+    public Text nameText;
+    public Text giftInfoText;
+    public void SetGiftUI(Sprite image)
+    {
+        giftImage.sprite = image; // 이미지 설정
+    }
+    public void SetGiftPrice(string price)
+    {
+        priceText.text = price;
+    }
+    public void SetLikeAbility(string likeAbility)
+    {
+        likeAbilityText.text = likeAbility;
+    }
+    public void SetNameText(string name)
+    {
+        nameText.text = name;
+    }
+    public void SetGiftInfo(string giftInfo)
+    {
+        giftInfoText.text = giftInfo.Replace("\\n", "\n");
+    }
+
+}
