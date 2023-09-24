0733ff89f622225fcbcdf4bff6ab602d7b4d9f45
이정민 <jungmin5789@gmail.com>
2023-09-24 21:47:45 +0900

    GiftshopUI 구매 결과 창 추가, 애니메이션 및 파티클 추가, 대사 타이핑 CS 추가

---
+++ projectm3/projectm3/Assets/Common/Scripts/TypingEffect.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TypingEffect : MonoBehaviour
+{
+    public Text textComponent;
+    public string fullText;
+    public float typingSpeed = 0.1f;
+
+    private Coroutine typingCoroutine; // 코루틴 참조를 저장할 변수
+
+    private void OnEnable()
+    {
+        // 오브젝트가 활성화될 때 타이핑 효과를 다시 시작
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+        textComponent.text = ""; // 텍스트 초기화
+        typingCoroutine = StartCoroutine(TypeText());
+    }
+
+    private void OnDisable()
+    {
+        // 오브젝트가 비활성화될 때 타이핑 효과를 중지
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+    }
+
+    private void Start()
+    {
+        // 초기에 타이핑 효과를 시작하지 않음
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in fullText)
+        {
+            textComponent.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
