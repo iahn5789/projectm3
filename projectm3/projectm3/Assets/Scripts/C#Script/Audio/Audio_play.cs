6ab177fc1b20a8d794c54d9bbf6152afa174ac51
98싸이월드얼짱장범준 <wkdqjawns777@gmail.com>
2023-10-11 22:26:36 +0900

    200_9) 선물상점 효과음 적용

---
+++ projectm3/projectm3/Assets/Scripts/C#Script/Audio/Audio_play.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_play : MonoBehaviour
+{
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void play_sound()
+    {
+        audio.Play();
+    }
+}
