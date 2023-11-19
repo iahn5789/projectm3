c4559c1b7b141fd746f4bee10985c0902b289c4e
KangSeokChan <ksc7299@naver.com>
2023-11-19 21:57:27 +0900

    80_1 : 후원자 리스트 작성 완

--- projectm3/projectm3/Assets/Common/Scripts/CreditUI.cs
+++ projectm3/projectm3/Assets/Common/Scripts/CreditUI.cs
@@ -9,6 +9,8 @@ public class CreditUI : MonoBehaviour
     public Button Button_We;
     public Button Button_Help;
     public Button Button_Spon;
+    public RectTransform Help_Rect;
+    public RectTransform Spon_Rect;
 
     private void Start()
     {
@@ -23,6 +25,7 @@ public class CreditUI : MonoBehaviour
         // Button_Help 클릭 시 Help를 활성화하고 다른오브젝트를 비활성화합니다.
         Button_Help.onClick.AddListener(() =>
         {
+            SetRectTransform(Help_Rect);
             We.SetActive(false);
             Help.SetActive(true);
             Spon.SetActive(false);
@@ -31,9 +34,15 @@ public class CreditUI : MonoBehaviour
         // Button_Spon 클릭 시 Spon를 활성화하고 다른오브젝트를 비활성화합니다.
         Button_Spon.onClick.AddListener(() =>
         {
+            SetRectTransform(Spon_Rect);
             We.SetActive(false);
             Help.SetActive(false);
             Spon.SetActive(true);
         });
+    }
+    public void SetRectTransform(RectTransform rectTransform)
+    {
+        float x = rectTransform.anchoredPosition.x;
+        rectTransform.anchoredPosition = new Vector3(x, 0, 0);
     }
 }
---
+++ projectm3/projectm3/Assets/Scripts/C#Script/CloneSponPrefabManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloneSponPrefabManager : MonoBehaviour
+{
+    public GameObject textPrefab; // 텍스트를 표시할 기본 GameObject (Text 컴포넌트 포함)
+    public TextAsset SponTextString;
+    private List<string> items; // 표시할 텍스트들의 리스트
+    private List<GameObject> clones = new List<GameObject>(); // 클론된 GameObject들을 추적하기 위한 리스트    
+    private string Separator = "|^%$";
+    private bool On = false;
+
+    // Update is called once per frame
+    public void ClonePrefab()
+    {
+        if (!On)
+        {
+            items = SplitSponText();
+            foreach (var item in items)
+            {
+                // 기본 GameObject를 복제
+                var clone = Instantiate(textPrefab, transform);
+
+                // 복제된 GameObject의 Text 컴포넌트를 찾아 텍스트 설정
+                var textComponent = clone.GetComponent<Text>();
+                if (textComponent != null)
+                {
+                    textComponent.text = item;
+                }
+                clones.Add(clone);
+            }
+            On = true;
+        }
+    }
+    public List<string> SplitSponText()
+    {
+        List<string> stringList = new List<string>();
+            
+        string[] splitItems = SponTextString.text.Split(new string[] { Separator }, System.StringSplitOptions.RemoveEmptyEntries);
+        foreach (var item in splitItems)
+        {
+            stringList.Add(item.Trim()); 
+        }
+
+        return stringList;
+    }
+    public void DestroyClones()
+    {
+        foreach (var clone in clones)
+        {
+            Destroy(clone);
+        }
+        clones.Clear();
+        On = false;
+    }
+}
