37ca1eaab6d1e547b69c5c314511f77ad2cc00e3
이정민 <jungmin5789@gmail.com>
2023-12-21 19:23:03 +0900

    1000_40 : 강석찬 작업 pussy

---
+++ projectm3/projectm3/Assets/Scripts/C#Script/StoryStartEndManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryStartEndManager : MonoBehaviour
+{
+    private ICustomVariableManager variableManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void NextScript()
+    {
+        variableManager?.SetVariableValue("Player_name", "정민");
+    }
+}
