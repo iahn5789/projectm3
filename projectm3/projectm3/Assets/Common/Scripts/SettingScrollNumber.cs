cfa09a6ad5a61430a7424feef16a0ae899c51843
이정민 <jungmin5789@gmail.com>
2024-01-09 04:54:24 +0900

    40_1 : 설정하기 슬라이더 수치 표시

---
+++ projectm3/projectm3/Assets/Common/Scripts/SettingScrollNumber.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingScrollNumber : MonoBehaviour
+{
+   public Slider exSlider;
+   public TextMeshProUGUI sliderText;
+
+   public void ChangeSlider()
+   {
+        sliderText.text = exSlider.value.ToString();
+   }
+}
