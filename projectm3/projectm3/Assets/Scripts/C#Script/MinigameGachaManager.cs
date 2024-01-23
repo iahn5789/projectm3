7dba7b8cba36b85b47f27cde014997228069c0d2
KangSeokChan <ksc7299@naver.com>
2024-01-24 00:11:01 +0900

    MG_2 : 대사 랜덤 출력 뽑기 시 대사도 넣어둠

--- projectm3/projectm3/Assets/Scripts/C#Script/MinigameGachaManager.cs
+++ projectm3/projectm3/Assets/Scripts/C#Script/MinigameGachaManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Naninovel;
+using Naninovel.Commands;
 
 public class MinigameGachaManager : MonoBehaviour
 {
@@ -15,18 +17,57 @@ public class MinigameGachaManager : MonoBehaviour
     public GameObject[] SelectedLight; // 4, 3, 2, 1
     public GameObject[] Scaffolding; // 2개 -1, -2
     public GameObject Arrow;
+    public Text DialogText;
+    private Dictionary<int, (string, int, int)> line;
+    public Image charactorFace; // Result창 캐릭터 얼굴
+    public Image charactorBody; // Result창 캐릭터 몸
+    public Sprite[] FaceList; // Result창 캐릭터 얼굴 리스트
+    public Sprite[] BodyList; // Result창 캐릭터 몸 리스트
     private Stack<int> PegsA;
     private Stack<int> PegsB;
     private Stack<int> PegsC;
     private int Selected_block = -1;
     private int Selected_Pegs = -1;
     private int N_Round = 0;
+    private ICustomVariableManager variableManager;
+    // 20회 도달시 뽑기 버튼 강제 클릭이 필요함 
+    public void Start()
+    {
+        variableManager = Engine.GetService<ICustomVariableManager>();
+        line = new Dictionary<int, (string, int, int)>();
 
+        line.Add(1, ("대사1", 0, 0));
+        line.Add(2, ("대사2", 0, 3));
+        line.Add(3, ("대사3", 2, 1));
+        line.Add(4, ("대사4", 1, 2));
+        line.Add(5, ("대사5", 0, 0));
+        line.Add(6, ("대사6", 0, 3));
+        line.Add(7, ("대사7", 2, 1));
+        line.Add(8, ("대사8", 1, 2));
+        line.Add(9, ("대사9", 2, 1));
+        line.Add(10, ("대사10", 1, 2));
+        line.Add(11, ("뽑기대사1", 1, 2));
+        line.Add(12, ("뽑기대사2", 2, 1));
+        line.Add(13, ("뽑기대사3", 0, 0));
+    }
+    private void GetLine(int key)
+    {
+        if(line.ContainsKey(key)) // 해당 키가 myTable 딕셔너리에 있는지 확인
+        {
+            var tupleValue = line[key];
+            DialogText.text = tupleValue.Item1;
+            charactorBody.sprite = BodyList[tupleValue.Item2];
+            charactorFace.sprite = FaceList[tupleValue.Item3];
+        }
+    }
     public void StartGame(int Round)
     {
         N_Round = Round;
         StageText.text = Round.ToString();
         
+        int randomValue = Random.Range(1, 11);
+        GetLine(randomValue);
+
         // 초기화 및 시작 위치 설정
         PegsA = new Stack<int>();
         PegsB = new Stack<int>();
@@ -77,14 +118,24 @@ public class MinigameGachaManager : MonoBehaviour
 
         // UpdatePegs 메서드 호출하여 위치 업데이트
         UpdatePegs();
+        InvokeRepeating("CallGetLine", 0, 10);
     }
-
+    // 10초마다 호출될 함수
+    private void CallGetLine()
+    {
+        int randomValue = Random.Range(1, 11);
+        GetLine(randomValue);
+    }
     public void UpdatePegs()
     {
         CountText.text = Count.ToString();
         UpdatePegsOnPeg(PegsA, 1);
         UpdatePegsOnPeg(PegsB, 2);
         UpdatePegsOnPeg(PegsC, 3);
+        if (Count >= ClearCount)
+        {
+            Catch();
+        }
     }
     private void UpdatePegsOnPeg(Stack<int> peg, int pegNumber)
     {
@@ -105,14 +156,27 @@ public class MinigameGachaManager : MonoBehaviour
             rectTransform.anchoredPosition = new Vector2(xPos, -102f + (pegsArray.Length - 1 - i) * 100f);
         }
     }
+    public void UIReset()
+    {
+        SelectedLight[0].SetActive(false);
+        SelectedLight[1].SetActive(false);
+        SelectedLight[2].SetActive(false);
+        SelectedLight[3].SetActive(false);
+        Arrow.SetActive(false);
+    }
     public void ResetGame()
     {
+        UIReset();
+        CancelInvoke("CallGetLine");
         // 옮긴 횟수 조절
         StartGame(N_Round);
     }
     public void Catch()
     {
-        // 옮긴 횟수 조절
+        CancelInvoke("CallGetLine");
+        // 랜덤 11~13 사이 값이 나와야함 그걸로 GetLine(int) 호출
+        int randomValue = Random.Range(11, 14);
+        GetLine(randomValue);
         if (N_Round == 1)
         {
             // PegsC에 원반이 1, 2, -1, -2 순서로 있을 때의 동작
