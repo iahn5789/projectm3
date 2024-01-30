64d92470ce2dbe58d897afa8256213a929963c5b
KangSeokChan <ksc7299@naver.com>
2024-01-31 00:47:42 +0900

    90_2 : 대화스크립트 중간에 들어가서 이야기 진행 후 다시 목록으로 돌아올 수 있게 개발, 우측 메뉴기능 제거,

---
+++ projectm3/projectm3/Assets/Scripts/C#Script/FlashBackManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using Naninovel.Commands;
+using Naninovel;
+using Naninovel.UI;
+
+public class FlashBackManager : MonoBehaviour
+{
+    private const string titleLabel = "OnNewGame";
+
+    [Tooltip("새 게임을 시작할 때 상태 재설정에서 제외하는 서비스")]
+    [SerializeField] private string[] excludeFromReset = Array.Empty<string>();
+
+    private string startScriptName;
+    private IScriptPlayer scriptPlayer;
+    private IStateManager stateManager;
+    private IScriptManager scriptManager;
+    public PlayScript playScript;
+    public GameObject Unlock;
+    
+    public void Awake()
+    {
+        scriptManager = Engine.GetService<IScriptManager>();
+        startScriptName = scriptManager.StartGameScriptName;
+        scriptPlayer = Engine.GetService<IScriptPlayer>();
+        stateManager = Engine.GetService<IStateManager>(); // stateManager 초기화 추가
+    }
+
+    public async void OnButtonClick()
+    {
+        if (Unlock.activeSelf) // Unlock GameObject가 현재 활성화 중인지 확인
+        {
+            stateManager.ResetStateAsync(excludeFromReset,
+                () => scriptPlayer.PreloadAndPlayAsync(startScriptName)).Forget();
+            // playScript.Play();
+            Debug.Log("Unlock");
+        }
+        Debug.Log("OnButtonClick");
+    }
+}
