92c2b3a7197447953c740356eeee76e7c65cf444
KangSeokChan <ksc7299@naver.com>
2024-03-03 21:06:12 +0900

    EC_2 : Dialog text 수정

--- projectm3/projectm3/Assets/Scripts/C#Script/EndingDialogueManager.cs
+++ projectm3/projectm3/Assets/Scripts/C#Script/EndingDialogueManager.cs
@@ -8,9 +8,11 @@ public class EndingDialogueManager : MonoBehaviour
     public GameObject CharactorDialog;
     public Text CharactorName;
 
-    public void SetDialogText(string text)
+    public void SetDialogText(string dialog_text)
     {
-        CharactorDialog.GetComponent<TypingEffect>().fullText = text;
+        CharactorDialog.SetActive(false);
+        CharactorDialog.GetComponent<TypingEffect>().fullText = dialog_text;
+        CharactorDialog.SetActive(true);
     }
     public void SetDialogName(string name)
     {
