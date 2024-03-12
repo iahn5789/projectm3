4c367c60c08082f85dbaf6407d4a7d8040e43d71
KangSeokChan <ksc7299@naver.com>
2024-03-12 21:18:16 +0900

    1000_123 : 자동읽기 진행 중 자동읽기 버튼 다시 한번 클릭 시 자동진행 아이콘 사라지지 않음. 다른 버튼 누를 시 사라짐

--- projectm3/projectm3/Assets/Common/Scripts/MenuInOut.cs
+++ projectm3/projectm3/Assets/Common/Scripts/MenuInOut.cs
@@ -7,10 +7,11 @@ public class MenuInOut : MonoBehaviour
 {
     public static Action target;
 
-    public Animator animator; // 애니메이터 컴포넌트
+    public Animator MenuAnimator; // 애니메이터 컴포넌트
+    public Animator AutoAnimator;
     public bool isMenuVisible = false; // 메뉴가 보이는 상태를 나타내는 변수
-
     private bool isFirstEscape = true; // 첫 ESC 입력 여부를 나타내는 변수
+    private bool isFirstShowActive = false; // FirstShow 트리거 활성화 여부를 나타내는 변수
 
     private void Awake()
     {
@@ -22,17 +23,33 @@ public class MenuInOut : MonoBehaviour
         // 현재 상태에 따라 애니메이션 전환
         if (!isMenuVisible)
         {
-            animator.SetTrigger("MenuUIOut"); // MenuUIOut 애니메이션 재생
+            MenuAnimator.SetTrigger("MenuUIOut"); // MenuUIOut 애니메이션 재생
         }
         else
         {
-            animator.SetTrigger("MenuUI"); // MenuUI 애니메이션 재생
+            MenuAnimator.SetTrigger("MenuUI"); // MenuUI 애니메이션 재생
         }
 
         // 메뉴 상태 변경
         isMenuVisible = !isMenuVisible;
     }
 
+    public void SetActiveAutoAnim()
+    {
+        // isFirstShowActive의 상태에 따라 애니메이터의 트리거 설정
+        if (!isFirstShowActive)
+        {
+            MenuAnimator.ResetTrigger("Hide"); // MenuUI 애니메이션 재생
+            AutoAnimator.SetTrigger("FirstShow");
+            isFirstShowActive = true; // FirstShow가 활성화되어 있음을 나타냄
+        }
+        else
+        {
+            MenuAnimator.ResetTrigger("FirstShow"); // MenuUI 애니메이션 재생
+            AutoAnimator.SetTrigger("Hide");
+            isFirstShowActive = false; // Hide가 활성화되어 있음을 나타냄
+        }
+    }
     private void Update()
     {
         // ESC 키가 눌렸을 때
