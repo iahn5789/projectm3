1f91a7682cbea16fc5a73ec7fd60e802f004ef91
KangSeokChan <ksc7299@naver.com>
2024-03-19 20:56:08 +0900

    EC_4 : 대물(EndingUI Text Up) 넣기

--- projectm3/projectm3/Assets/Scripts/C#Script/EndingDialogCreditManager.cs
+++ projectm3/projectm3/Assets/Scripts/C#Script/EndingDialogCreditManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class EndingDialogCreditManager : MonoBehaviour
 {
-    public Text creditText; // 크레딧 텍스트를 위한 Text 컴포넌트
+    public GameObject credit; // 크레딧 텍스트를 위한 Text 컴포넌트
     public float scrollSpeed = 20f; // 텍스트 스크롤 속도
 
     public void StartScroll()
@@ -15,13 +15,13 @@ public class EndingDialogCreditManager : MonoBehaviour
 
     IEnumerator ScrollCredits()
     {
-        float posY = creditText.transform.position.y;
+        float posY = credit.transform.position.y;
 
         // 텍스트가 위로 스크롤되는 동안 무한 반복
         while (true)
         {
             posY += scrollSpeed * Time.deltaTime; // 텍스트 위치 업데이트
-            creditText.transform.position = new Vector2(creditText.transform.position.x, posY);
+            credit.transform.position = new Vector2(credit.transform.position.x, posY);
             
             yield return null; // 다음 프레임까지 기다림
         }
